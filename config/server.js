4cd7a84f854fb1f99fe834dc64694968821f2f14
jakub.granos <jakub.granos@chop-chop.org>
2022-04-24 18:26:14 +0200

    make it better

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+    host: process.env.HOST
+    port: process.env.PORT
+    proxy: true,
+    app: {
+        keys: process.env.APP_KEYS,
+    },
 });
