9fc93f287fee868bc7092b7ee7cae7c794c5e964
jakub.granos <jakub.granos@chop-chop.org>
2022-04-26 12:29:09 +0200

    make it better

--- config/database.js
+++ config/database.js
@@ -1,38 +1,11 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = ({ env }) => {
-    const configs = {
-        LOCAL: {
-            connection: {
-                client: "sqlite",
-                connection: {
-                    filename: path.join(
-                        __dirname,
-                        "..",
-                        env("DATABASE_FILENAME", ".tmp/data.db")
-                    ),
-                },
-                useNullAsDefault: true,
-            },
+module.exports = ({ env }) => ({
+    connection: {
+        client: 'sqlite',
+        connection: {
+            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
         },
-
-        REMOTE: {
-            connection: {
-                client: "postgres",
-                connection: {
-                    host: env("DB_HOST"),
-                    port: env.int("DB_PORT"),
-                    database: env("DB_NAME"),
-                    user: env("DB_USER"),
-                    password: env("DB_PASS"),
-                    ssl: {
-                        rejectUnauthorized: false
-                    },
-                },
-                debug: true,
-            },
-        },
-    };
-
-    return configs[env("DATABASE_MODE")];
-};
+        useNullAsDefault: true,
+    },
+});
