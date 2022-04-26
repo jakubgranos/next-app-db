c985c31e8a9f55eed3ad19e1ffa1fc2539671821
jakub.granos <jakub.granos@chop-chop.org>
2022-04-26 15:23:08 +0200

    push changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-    connection: {
-        client: 'sqlite',
-        connection: {
-            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+module.exports = ({ env }) => {
+    const configs = {
+        LOCAL: {
+            connection: {
+                client: "sqlite",
+                connection: {
+                    filename: path.join(
+                        __dirname,
+                        "..",
+                        env("DATABASE_FILENAME", ".tmp/data.db")
+                    ),
+                },
+                useNullAsDefault: true,
+            },
         },
-        useNullAsDefault: true,
-    },
-});
+
+        REMOTE: {
+            connection: {
+                client: "postgres",
+                connection: {
+                    host: env("DB_HOST", "127.0.0.1"),
+                    port: env.int("DB_PORT", 5432),
+                    database: env("DB_NAME"),
+                    user: env("DB_USER"),
+                    password: env("DB_PASS"),
+                    schema: env("DB_SCHEMA"),
+                    ssl: {
+                        ca: env('DB_CA')
+                    }
+                },
+            },
+        },
+    };
+
+    return configs[env("DATABASE_MODE")];
+};
