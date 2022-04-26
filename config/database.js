c32036294843fd6c16e7c555475ff45c25137110
jakub.granos <jakub.granos@chop-chop.org>
2022-04-26 12:25:30 +0200

    make it better

--- config/database.js
+++ config/database.js
@@ -29,7 +29,7 @@ module.exports = ({ env }) => {
                         rejectUnauthorized: false
                     },
                 },
-                debug: false,
+                debug: true,
             },
         },
     };
